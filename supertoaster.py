9c2a2b9f5a2e78f47c76c0c86dbd05f193df12ab
caevee <pister.alex.cv@gmail.com>
2022-06-22 12:43:05 +0200

    gitignore

--- supertoaster.py
+++ supertoaster.py
@@ -4,10 +4,12 @@ from toaster import Toaster
 class SuperToaster(Toaster):
 
     def __init__(self, farbe, schaechte):
+        # super() sorgt dafür das die Properties und Methoden von Toaster übernommen werden.
         super().__init__(farbe, schaechte)
         self._temperatur = 0
 
 
+    # Override der toasten() Methode. Grund: Check der Temperatur zum abbrechen des Toastvorgangs.
     def toasten(self):
         if self._anzahl_toasts == 0:
             print("Geht nicht. Kein Toast drin.")
@@ -17,3 +19,9 @@ class SuperToaster(Toaster):
             sleep(self._toastzeit)
             self._toasts_zustand = 2
             print("Fertig")
+
+test = SuperToaster("Blau", 3)
+
+test.zeit_einstellen(5)
+test.toast_reintun(2)
+test.toasten()
